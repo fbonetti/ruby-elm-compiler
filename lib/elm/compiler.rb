618d8c85229d77d41611ca2ea98c807a60e43c9d
Frank Bonetti <frank.r.bonetti@gmail.com>
2015-12-25 12:06:14 -0600

    Output string

--- lib/elm/compiler.rb
+++ lib/elm/compiler.rb
@@ -5,19 +5,19 @@ require "tempfile"
 module Elm
   class Compiler
     def self.compile(pathname)
-      Dir.mktmpdir do |dir|
-        Dir.chdir(dir)
+      output = ""
 
-        File.open("elm.js", "w") do |tempfile|
-          Open3.popen3("elm-make", pathname.to_s, "--yes", "--output", tempfile.path) do |_stdin, stdout, stderr, wait_thr|
-            if wait_thr.value.exitstatus != 0
-              raise stderr.gets
-            end
+      Tempfile.open(["elm", ".js"]) do |tempfile|
+        Open3.popen3("elm-make", pathname.to_s, "--yes", "--output", tempfile.path) do |_stdin, stdout, stderr, wait_thr|
+          if wait_thr.value.exitstatus != 0
+            raise stderr.gets
           end
-
-          tempfile.read
         end
+
+        output = File.read tempfile.path
       end
+
+      output
     end
   end
 end
