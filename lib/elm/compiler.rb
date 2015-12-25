37ee87e2ac191dfba9f63fec73ce6f4260becca0
Frank Bonetti <frank.r.bonetti@gmail.com>
2015-12-25 12:00:38 -0600

    Adds compiler class

--- lib/elm/compiler.rb
+++ lib/elm/compiler.rb
@@ -1,7 +1,23 @@
 require "elm/compiler/version"
+require "open3"
+require "tempfile"
 
 module Elm
-  module Compiler
-    # Your code goes here...
+  class Compiler
+    def self.compile(pathname)
+      Dir.mktmpdir do |dir|
+        Dir.chdir(dir)
+
+        File.open("elm.js", "w") do |tempfile|
+          Open3.popen3("elm-make", pathname.to_s, "--yes", "--output", tempfile.path) do |_stdin, stdout, stderr, wait_thr|
+            if wait_thr.value.exitstatus != 0
+              raise stderr.gets
+            end
+          end
+
+          tempfile.read
+        end
+      end
+    end
   end
 end
--- lib/elm/compiler/version.rb
+++ lib/elm/compiler/version.rb
@@ -1,5 +1,5 @@
 module Elm
-  module Compiler
+  class Compiler
     VERSION = "0.1.0"
   end
 end
