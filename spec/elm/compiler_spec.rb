68388211fc03dfca5f21d5fd13f537904d97ab92
Frank Bonetti <frank.r.bonetti@gmail.com>
2015-12-26 13:54:16 -0600

    Adds additional tests

--- spec/elm/compiler_spec.rb
+++ spec/elm/compiler_spec.rb
@@ -14,18 +14,40 @@ describe Elm::Compiler do
       expect(&code).to raise_exception(Elm::Compiler::ExecutableNotFound)
     end
 
-    it 'should return a string if no output_path is given' do
-      output = Elm::Compiler.compile(test_file)
-      expect(output).to be_instance_of(String)
-      expect(output.empty?).to be(false)
+    context 'output_path NOT given' do
+      it 'should return a string if no output_path is given' do
+        output = Elm::Compiler.compile(test_file)
+        expect(output).to be_instance_of(String)
+        expect(output.empty?).to be(false)
+      end
+
+      it 'should accept a string or an array of strings' do
+        output = Elm::Compiler.compile([test_file])
+        expect(output).to be_instance_of(String)
+        expect(output.empty?).to be(false)
+      end
     end
 
-    it 'should write to the given output_path' do
-      File.delete('elm.js') if File.exist?('elm.js')
-      output = Elm::Compiler.compile(test_file, 'elm.js')
-      expect(output).to be_nil
-      expect(File.exist?('elm.js')).to be(true)
-      File.delete('elm.js')
+    context 'output_path given' do
+      before do
+        File.delete('elm.js') if File.exist?('elm.js')
+      end
+
+      after do
+        File.delete('elm.js')
+      end
+
+      it 'should write to the given output_path' do
+        output = Elm::Compiler.compile(test_file, 'elm.js')
+        expect(output).to be_nil
+        expect(File.exist?('elm.js')).to be(true)
+      end
+
+      it 'should accept a string or an array of strings' do
+        output = Elm::Compiler.compile([test_file], 'elm.js')
+        expect(output).to be_nil
+        expect(File.exist?('elm.js')).to be(true)
+      end
     end
   end
 end
