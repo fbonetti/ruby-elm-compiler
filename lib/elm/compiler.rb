d7d0ccb8d88d16d65eefb31ac731042c36f72368
Valdes, Noriel <nvaldes@c21stores.com>
2016-12-19 10:17:57 -0500

    Added debug flag
    Add byebugs because I don't know how to debug gems without running them
    
    Remove byebug, add debug as a separate arg instead of appending it to the output path (probably isn't using string concatenation, or at least isn't quite so simple).
    
    DRYed up the debug conditional, and changed != 0 to .nonzero? in the fail condition at rubocop's suggestion.
    
    Un-DRY...maybe test will pass now?
    
    Bump versions in travis.yml and elm-package.json to 0.18.0, add a test for the inclusion of debug functions in compiler output.
    
    Actually DRY the compiler function

--- lib/elm/compiler.rb
+++ lib/elm/compiler.rb
@@ -5,13 +5,13 @@ require 'tempfile'
 module Elm
   class Compiler
     class << self
-      def compile(elm_files, output_path: nil, elm_make_path: "elm-make")
+      def compile(elm_files, output_path: nil, elm_make_path: "elm-make", debug: false)
         fail ExecutableNotFound unless elm_executable_exists?(elm_make_path)
 
         if output_path
-          elm_make(elm_make_path, elm_files, output_path)
+          elm_make(elm_make_path, elm_files, output_path, debug)
         else
-          compile_to_string(elm_make_path, elm_files)
+          compile_to_string(elm_make_path, elm_files, debug)
         end
       end
 
@@ -23,15 +23,17 @@ module Elm
         false
       end
 
-      def compile_to_string(elm_make_path, elm_files)
+      def compile_to_string(elm_make_path, elm_files, debug)
         Tempfile.open(['elm', '.js']) do |tempfile|
-          elm_make(elm_make_path, elm_files, tempfile.path)
+          elm_make(elm_make_path, elm_files, tempfile.path, debug)
           return File.read tempfile.path
         end
       end
 
-      def elm_make(elm_make_path, elm_files, output_path)
-        Open3.popen3({"LANG" => "en_US.UTF8" }, elm_make_path, *elm_files, '--yes', '--output', output_path) do |_stdin, _stdout, stderr, wait_thr|
+      def elm_make(elm_make_path, elm_files, output_path, debug)
+        args = [{"LANG" => "en_US.UTF8" }, elm_make_path, *elm_files, '--yes', '--output', output_path]
+        args << "--debug" if debug
+        Open3.popen3(*args) do |_stdin, _stdout, stderr, wait_thr|
           fail CompileError, stderr.gets(nil) if wait_thr.value.exitstatus != 0
         end
       end
