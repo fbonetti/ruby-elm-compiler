8cfa03ce1fd1a2b4c0e9dce138ff3d63713cb66c
Dan Luria <dan@dotdan.com>
2019-02-02 21:35:19 -0500

    Upgrade to Elm 0.19

--- lib/elm/compiler.rb
+++ lib/elm/compiler.rb
@@ -5,34 +5,33 @@ require 'tempfile'
 module Elm
   class Compiler
     class << self
-      def compile(elm_files, output_path: nil, elm_make_path: "elm-make", debug: false)
-        fail ExecutableNotFound unless elm_executable_exists?(elm_make_path)
+      def compile(elm_files, output_path: nil, elm_path: nil, debug: false)
+        elm_path ||= `which elm`.strip
+        fail ExecutableNotFound unless elm_executable_exists?(elm_path)
 
         if output_path
-          elm_make(elm_make_path, elm_files, output_path, debug)
+          elm_make(elm_path, elm_files, output_path, debug)
         else
-          compile_to_string(elm_make_path, elm_files, debug)
+          compile_to_string(elm_path, elm_files, debug)
         end
       end
 
       private
 
-      def elm_executable_exists?(elm_make_path)
-        Open3.popen2(elm_make_path){}.nil?
-      rescue Errno::ENOENT, Errno::EACCES
-        false
+      def elm_executable_exists?(elm_path)
+        File.exist?(elm_path)
       end
 
-      def compile_to_string(elm_make_path, elm_files, debug)
+      def compile_to_string(elm_path, elm_files, debug)
         Tempfile.open(['elm', '.js']) do |tempfile|
-          elm_make(elm_make_path, elm_files, tempfile.path, debug)
+          elm_make(elm_path, elm_files, tempfile.path, debug)
           return File.read tempfile.path
         end
       end
 
-      def elm_make(elm_make_path, elm_files, output_path, debug)
-        args = [{"LANG" => "en_US.UTF8" }, elm_make_path, *elm_files, '--yes', '--output', output_path]
-        args << "--debug" if debug
+      def elm_make(elm_path, elm_files, output_path, debug)
+        args = [{'LANG' => 'en_US.UTF8'}, elm_path, 'make', *elm_files, '--output', output_path]
+        args << '--debug' if debug
         Open3.popen3(*args) do |_stdin, _stdout, stderr, wait_thr|
           fail CompileError, stderr.gets(nil) if wait_thr.value.exitstatus != 0
         end
